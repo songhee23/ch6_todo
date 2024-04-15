912549fe788a8a3fe8a61c4640c5b97be5f55649
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-15 17:18:23 +0900

    container 만듬

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #eeeeee;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+
+    </Container>
+  )
 }
 
 export default App;
