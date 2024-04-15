0a4c36e7c625797a8e08eecac94e7bb9c98b2889
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-15 17:46:47 +0900

    button - conmponents분리

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled'
 import { Title } from 'components/Title';
+import { Button } from 'components/Button';
  
 const Container = styled.div`
   height: 100vh;
@@ -12,30 +13,11 @@ const Container = styled.div`
   background-color: #eeeeee;
 `;
 
-
-const Button = styled.button`
-  border: 0;
-  color: #ffffff;
-  background-color: #ff5722;
-  cursor: pointer;
-  padding: 8px 16px;
-  border-radius: 4px;
-
-  &:hover {
-    background-color: #ff5722;
-    opacity: 0.8;
-  }
-
-  &:active {
-    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
-  }
-`;
-
 function App() {
   return (
     <Container>
       <Title label="할 일 목록" />
-      <Button onClick={() => console.log('test')}>삭제</Button>
+      <Button />
     </Container>
   )
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+const Container = styled.button`
+  border: 0;
+  color: #ffffff;
+  background-color: #ff5722;
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #ff5722;
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const Button = () => {
+    return (
+        <Container onClick={() => console.log('test')}>삭제</Container>
+    );
+  };
+  
