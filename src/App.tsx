f6ffa33b6f7b91060b41d791bb47f897a657c915
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-16 13:19:57 +0900

    삭제 버튼 기능 useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled'
 import { Title } from 'components/Title';
-import { ToDoList } from 'components/ToDoList'
+import { ToDoList } from 'components/ToDoList';
+import { useState } from 'react';
  
 const Container = styled.div`
   height: 100vh;
@@ -14,10 +15,20 @@ const Container = styled.div`
 `;
 
 function App() {
+  const [toDoList, setToDoList] = useState([
+    '리액트 공부하기',
+    '운동하기',
+    '책읽기'
+  ]);
+
+  const onDelete = (todo: string) => {
+    setToDoList(toDoList.filter((item) => item !== todo));
+  };
+
   return (
     <Container>
       <Title label="할 일 목록" />
-      <ToDoList toDoList={['리액트 공부하기', '운동하기', '책읽기']}/>
+      <ToDoList toDoList={toDoList} onDelete={onDelete}/>
     </Container>
   )
 }
